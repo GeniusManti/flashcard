948a5e7413606344c460e37098c84f5b51d27ce6
geniusmanti <kacper.kuzniarski@gmail.com>
2017-10-20 00:40:32 +0200

    added dependencies and main method

---
+++ src/main/java/com/kuzniarski/springbootstarter/Application.java
@@ -0,0 +1,15 @@
+package com.kuzniarski.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Kacper Kuźniarski on 20.10.2017.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
