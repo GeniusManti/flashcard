52eb4f112657f644d22415fa7685b25ae5bdf87c
kuzxnia <kacper.kuzniarski@gmail.com>
2018-04-20 10:55:36 +0200

    added domain class User and enum Role

---
+++ src/main/java/flashcard/application/domain/User.java
@@ -0,0 +1,23 @@
+package flashcard.application.domain;
+
+import flashcard.application.domain.enums.Role;
+import lombok.*;
+
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Builder
+public class User {
+
+    private String nick;
+    private String email;
+    private String phoneNumber;
+    private String password;
+    private boolean banned;
+    private Role role;
+
+
+}
---
+++ src/main/java/flashcard/application/domain/enums/Role.java
@@ -0,0 +1,21 @@
+package flashcard.application.domain.enums;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public enum  Role {
+    ADMIN("ADMIN"),
+    DISABLED("BANNED"),
+    USER("USER");
+
+    String name;
+
+    Role(String name) {
+        this.name = name;
+    }
+
+    public boolean equalsName(String otherName) {
+        return name.equals(otherName);
+    }
+}
