6ce046a434e4f392c82870699158458003cf6f34
kuzxnia <kacper.kuzniarski@gmail.com>
2018-04-23 13:19:20 +0200

    added userService implementation and ObjecNotFoundException

--- src/main/java/flashcard/web/services/UserService.java
+++ src/main/java/flashcard/web/services/UserService.java
@@ -1,11 +1,13 @@
 package flashcard.web.services;
 
+import flashcard.web.DTO.request.CreateUserRequestDTO;
 import flashcard.web.DTO.response.UserResponseDTO;
 import org.springframework.http.ResponseEntity;
 
 public interface UserService {
-    UserResponseDTO createUser(long id);
-    UserResponseDTO readUser(long id);
-    UserResponseDTO deleteUser(long id);
+    UserResponseDTO createUser(CreateUserRequestDTO userRequestDTO);
+    UserResponseDTO readUserById(long id);
+    void deleteUser(long id);
+    void deleteAllUser();
     UserResponseDTO readUserByNick(String nick);
 }
--- src/main/java/flashcard/web/services/UserServiceImpl.java
+++ src/main/java/flashcard/web/services/UserServiceImpl.java
@@ -1,30 +1,57 @@
 package flashcard.web.services;
 
+import flashcard.application.domain.User;
+import flashcard.application.exceptions.ObjectNotFoundException;
+import flashcard.web.DTO.request.CreateUserRequestDTO;
 import flashcard.web.DTO.response.UserResponseDTO;
-import org.apache.catalina.User;
+import flashcard.web.mappers.UserMapper;
+import flashcard.web.repositories.UserRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
-    @Override
-    public UserResponseDTO createUser(long id) {
-        return null;
+    private UserRepository userRepository;
+    private UserMapper userMapper;
+
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
     }
 
     @Override
-    public UserResponseDTO readUser(long id) {
-        return null;
+    public UserResponseDTO createUser(CreateUserRequestDTO userRequestDTO) {
+        User userToSave = userMapper.mapFromRequestDTO(userRequestDTO);
+
+        User createdUser = userRepository.save(userToSave);
+
+        return userMapper.mapToResponseDTO(createdUser);
     }
 
     @Override
-    public UserResponseDTO deleteUser(long id) {
-        return null;
+    public UserResponseDTO readUserById(long id) {
+        User foundUser = userRepository.readById(id)
+                .orElseThrow(() -> new ObjectNotFoundException(User.class, id));
+
+        return userMapper.mapToResponseDTO(foundUser);
     }
 
     @Override
     public UserResponseDTO readUserByNick(String nick) {
-        return null;
+        User foundUser = userRepository.readByNick(nick)
+                .orElseThrow(() -> new ObjectNotFoundException(User.class, "nick", nick));
+
+        return userMapper.mapToResponseDTO(foundUser);
+    }
+
+    @Override
+    public void deleteUser(long id) {
+        userRepository.delete(id);
+    }
+
+    @Override
+    public void deleteAllUser() {
+        userRepository.deleteAll();
     }
 }
