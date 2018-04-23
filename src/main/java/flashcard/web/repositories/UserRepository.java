5b998e687c7504e7ca05a1594fbcb6c7eeb7a698
kuzxnia <kacper.kuzniarski@gmail.com>
2018-04-23 12:28:04 +0200

    added basic userRepository and data.sql script

--- src/main/java/flashcard/web/repositories/UserRepository.java
+++ src/main/java/flashcard/web/repositories/UserRepository.java
@@ -1,12 +1,18 @@
 package flashcard.web.repositories;
 
 import flashcard.application.domain.User;
+import flashcard.web.DTO.response.UserResponseDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    Optional<User> readById(long id);
+    Optional<User> readByNick(String nick);
 
 }
